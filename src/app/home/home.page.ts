74d72618e79e60d0744ec3e1c13bca8b642bd9fb
NMarin <nmarinsiruela@gmail.com>
2018-12-10 14:40:36 +0000

    WIP: Create initial player mechanic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  newPlayer: string;
+  players: Array<string>;
+  constructor() {
+    this.newPlayer = '';
+    this.players = [];
+  }
+
+  setNewPlayer(name) {
+    if (name !== '') {
+      this.players.push(name);
+      this.newPlayer = '';
+    }
+  }
+
+  removePlayer(player) {
+    const index: number = this.players.indexOf(player);
+    if (index !== -1) {
+        this.players.splice(index, 1);
+    }
+  }
+
+  setPlayers() {
+    for (let x = 0; x < this.players.length; x++) {
+      console.log(this.players[x]);
+    }
+  }
+
 
 }
